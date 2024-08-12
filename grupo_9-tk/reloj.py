10c25adf4fd5dfbc2d38d93c9400f5c7c8f537e7
ncicka <ncicka@gmail.com>
2024-08-11 22:22:14 -0300

    Reloj en la esquina superior derecha

--- grupo_9-tk/reloj.py
+++ grupo_9-tk/reloj.py
@@ -3,7 +3,14 @@ import time
 
 ventana = tk.Tk()
 ventana.title('Reloj simple')
-ventana.geometry('400x200')
+ancho_ventana = 400
+alto_ventana = 100
+ancho_pantalla, alto_pantalla = ventana.winfo_screenmmwidth(), ventana.winfo_screenheight()
+posicion_x = ancho_pantalla - ancho_ventana
+posicion_y = alto_pantalla - alto_ventana
+
+ventana.geometry(f"{ancho_ventana}x{alto_ventana}-{posicion_x}-{posicion_y}")
+
 reloj = tk.Label(ventana, font = ('Arial', 60), bg = 'blue', fg = 'white') 
 
 def hora():
@@ -12,6 +19,6 @@ def hora():
     ventana.after(1000, hora)
     reloj.pack(anchor = 'center')
 
-""" hora()"""
+hora()
 
 ventana.mainloop()
